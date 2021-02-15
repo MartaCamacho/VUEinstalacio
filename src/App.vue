4a407961fa0bc2d167470ba7c32680982a045bcb
martacamacho <martacamachovargas@gmail.com>
2021-02-15 19:40:45 +0100

    done

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,6 @@
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script type="text/javascript" src="./Scripts/app.js"></script>
+<style src="./Styles/CSS/css/app.css"></style> 
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
